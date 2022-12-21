0365366f1b4ce8260d097c06db275b6f0d73c416
Stas Kraev <staskraev@catalyst.net.nz>
2022-12-21 15:16:24 +1300

    Capture greedy process command line

--- process_exporter.sh
+++ process_exporter.sh
@@ -4,6 +4,6 @@ while :; do
 
 	ps -haxo pid,rss,pcpu,comm | awk '{ print "process_rss{pid=\""$1"\",name=\""$4"\"} " $2 }' > /data/textfile_collector/process_rss.prom
 	ps -haxo pid,rss,pcpu,comm | awk '{ print "process_cpu{pid=\""$1"\",name=\""$4"\"} " $3 }' > /data/textfile_collector/process_cpu.prom
+	ps -haxo pid,rss,pcpu,comm,cmd | awk '{ if ( $3 > 80 ) { print $1, $3, $5} }' | tee -a /data/logs/process.log
 	sleep 30
-
 done
